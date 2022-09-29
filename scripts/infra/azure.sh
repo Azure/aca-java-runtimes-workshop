ee842f43df6c511962b4422312bb94421065e3e4
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-09-29 14:59:37 +0200

    Adds Postgres Connection String

--- scripts/infra/azure.sh
+++ scripts/infra/azure.sh
@@ -255,6 +255,20 @@ az containerapp env create \
     --querytext "select * from Statistics"
 # end::adocPostgresSelect[]
 
+# tag::adocPostgresConnectionString[]
+  POSTGRES_CONNECTION_STRING=$(
+    az postgres flexible-server show-connection-string \
+      --server-name "$POSTGRES_DB" \
+      --admin-user "$POSTGRES_DB_ADMIN" \
+      --admin-password "$POSTGRES_DB_PWD" \
+      --database-name "$POSTGRES_DB_SCHEMA" \
+      --query "connectionStrings.jdbc" \
+      --output tsv
+  )
+
+  echo "POSTGRES_CONNECTION_STRING=$POSTGRES_CONNECTION_STRING"
+# end::adocPostgresConnectionString[]
+
   echo "Environment '${environment}' of project '${project_name}' ready."
 }
 
