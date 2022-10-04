187d131409f609e25798f78d07664b9e758e54db
Julien Dubois <julien.dubois@gmail.com>
2022-10-04 17:13:22 +0200

    Configure TestContainers

--- springboot-app/src/test/java/io/containerapps/javaruntime/workshop/quarkus/springboot/SpringbootApplicationTests.java
+++ springboot-app/src/test/java/io/containerapps/javaruntime/workshop/quarkus/springboot/SpringbootApplicationTests.java
@@ -3,7 +3,11 @@ package io.containerapps.javaruntime.workshop.quarkus.springboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = {
+    "spring.datasource.url=jdbc:tc:postgresql:14-alpine://testcontainers/postgres",
+    "spring.datasource.username=postgres",
+    "spring.datasource.password=password"
+})
 class SpringbootApplicationTests {
 
 	@Test
