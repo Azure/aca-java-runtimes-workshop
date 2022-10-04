a74e746afd89dc97557f8c6fd12021ffd6ed8c8e
Julien Dubois <julien.dubois@gmail.com>
2022-10-04 22:30:47 +0200

    Setup GraalVM native build

--- springboot-app/src/main/java/io/containerapps/javaruntime/workshop/springboot/Statistics.java
+++ springboot-app/src/main/java/io/containerapps/javaruntime/workshop/springboot/Statistics.java
@@ -3,6 +3,7 @@ package io.containerapps.javaruntime.workshop.springboot;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+
 import java.time.Duration;
 import java.time.Instant;
 
