1d53336800752de2e4fe9411ed185db1342478b4
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-10-07 11:50:35 +0200

    remove sed from template

--- scripts/create-github-template.sh
+++ scripts/create-github-template.sh
@@ -21,14 +21,6 @@ rm -rf $TEMPLATE_HOME/SECURITY.md
 rm -rf $TEMPLATE_HOME/SUPPORT.md
 rm -rf $TEMPLATE_HOME/.github/workflows/*
 
-### Fix the registry name of the workflows
-sed 's/javaruntimesregistrysinedied/<YOUR_REGISTRY_URL>/' $TEMPLATE_HOME/.github/workflows/deploy.yml >> $TEMPLATE_HOME/.github/workflows/deploy-new.yml
-rm $TEMPLATE_HOME/.github/workflows/deploy.yml
-mv $TEMPLATE_HOME/.github/workflows/deploy-new.yml $TEMPLATE_HOME/.github/workflows/deploy.yml
-sed 's/javaruntimesregistrysinedied/<YOUR_REGISTRY_URL>/' $TEMPLATE_HOME/.github/workflows/deploy-native.yml >> $TEMPLATE_HOME/.github/workflows/deploy-native-new.yml
-rm $TEMPLATE_HOME/.github/workflows/deploy-native.yml
-mv $TEMPLATE_HOME/.github/workflows/deploy-native-new.yml $TEMPLATE_HOME/.github/workflows/deploy-native.yml
-
 ### Removing the Micronaut files
 rm -rf $TEMPLATE_HOME/micronaut-app/target
 rm -rf $TEMPLATE_HOME/micronaut-app/src/main/java/io/containerapps/javaruntime/workshop/micronaut/Micronaut*
