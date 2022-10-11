9d9dcc328d39ba05c0a81412c33a88a614efd68d
forax <forax@univ-mlv.fr>
2022-10-11 14:14:02 +0200

    fix wrong import oops

--- micronaut-app/src/main/java/io/containerapps/javaruntime/workshop/micronaut/MicronautResource.java
+++ micronaut-app/src/main/java/io/containerapps/javaruntime/workshop/micronaut/MicronautResource.java
@@ -5,7 +5,6 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
-import org.hibernate.AssertionFailure;
 
 import java.time.Duration;
 import java.time.Instant;
