c85936498b3ad3f8a11aca17e6582cd1cf0f6fad
Julien Dubois <julien.dubois@gmail.com>
2023-03-27 09:12:28 +0200

    Remove the Spring "hints" for native builds as they are not required anymore

--- springboot-app/src/main/java/io/containerapps/javaruntime/workshop/springboot/SpringbootApplication.java
+++ springboot-app/src/main/java/io/containerapps/javaruntime/workshop/springboot/SpringbootApplication.java
@@ -1,26 +1,13 @@
 package io.containerapps.javaruntime.workshop.springboot;
 
-import org.springframework.aot.hint.MemberCategory;
-import org.springframework.aot.hint.RuntimeHints;
-import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints(SpringbootApplication.MyRuntimeHints.class)
 public class SpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
-    static class MyRuntimeHints implements RuntimeHintsRegistrar {
-
-        @Override
-        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-            hints.reflection()
-                .registerType(org.hibernate.dialect.PostgreSQL95Dialect.class, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-        }
-    }
 }
